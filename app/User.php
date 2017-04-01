cd5d84f05ab2e3fcd5a5e5f0b3d5cf9f8f7e33e0
zgohanz <phamngocdung0211@gmail.com>
2017-04-01 20:01:35 +0700

    --

--- app/User.php
+++ app/User.php
@@ -73,6 +73,8 @@ class User extends Authenticatable
         }
         return false;
     }
+    /*updat status user 
+    */
     public function updateStatus($mobile_token,$online){
         return User::where('mobile_token',$mobile_token)->update(['status' => $online]);
 
